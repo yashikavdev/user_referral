889e4255f1a52c67ba3b3393c09404e650eb4461
Yashika <yvijayvargiyadev@gmail.com>
2023-06-08 16:13:21 +0530

    Added material UI and user invitation

---
+++ app/javascript/components/UI/Header.tsx
@@ -0,0 +1,79 @@
+import React, { MouseEvent, useEffect, useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+
+interface pageProp {
+  title:string,
+  handleClick : Function
+}
+
+function Header() {
+  const navigate = useNavigate();
+  const [pages, setpages] = useState<any>([]);
+  const isLoggedIn = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setpages([
+        {
+          title: "DashBoard",
+          handleClick: () => {
+            navigate("/dashboard");
+          },
+        },
+        {
+          title: "New Invitation",
+          handleClick: () => {
+            navigate("/new-invitation");
+          },
+        },
+        {
+          title: "Logout",
+          handleClick: () => {
+            navigate("/signin");
+            localStorage.removeItem('token');
+          },
+        },
+      ]);
+    } else {
+      setpages([{
+          title: "Sign In",
+          handleClick: () => {
+            navigate("/signin");
+          },
+        },
+        {
+          title: "Sign Up",
+          handleClick: () => {
+            navigate("/signup");
+          },
+        },
+      ]);
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <AppBar position="static" color="secondary">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' }}}>
+            {pages.map((page:pageProp) => (
+              <Button
+                key={page.title}
+                onClick={()=>page.handleClick()}
+                sx={{ my: 2, color: 'white', display: 'block'}}
+              >
+                {page.title}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Header;
---
+++ app/javascript/components/UserInvitation.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useFormik } from "formik";
+import { invitationSchema } from "../utils";
+import Error from "./error";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Header from './UI/Header';
+interface Invitation {
+  email: string;
+}
+
+const defaultTheme = createTheme();
+
+export default function UserInvitation() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const headers: any = {
+    'Content-Type': 'application/json',
+    'Authorization':`Bearer ${token}`
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: ""
+    },
+    validationSchema: invitationSchema,
+    onSubmit: (values: Invitation) => {
+      axios.post("/invitations.json", { "invitation": values }, { headers: headers })
+        .then((response) => {
+          navigate("/dashboard");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  });
+
+  const { values, handleChange, handleBlur, handleSubmit, errors, touched } =
+    formik;
+
+  return (
+    <>
+      <Header />
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography component="h1" variant="h5">
+              User Invitation
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.email}
+              />
+              <Error error={errors?.email} touched={touched?.email} />
+              <Button
+                color="secondary"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Send Invite
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
