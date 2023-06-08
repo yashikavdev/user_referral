40198481afe8a1e8d69b433e2e93693fbc495993
Yashika <yvijayvargiyadev@gmail.com>
2023-06-08 21:51:29 +0530

    Added urls

--- app/javascript/routers/index.tsx
+++ app/javascript/routers/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
 import DashBoard from "../components/Dashboard";
-import UserInvitation from "../components/UserInvitation";
 import PrivateRoute from "./privateRoute";
+import UserInvitation from "../components/UserInvitation";
+import Header from "../components/UI/Header";
 
 function Index() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route
           path="/dashboard"
           element={<PrivateRoute component={DashBoard} />}
